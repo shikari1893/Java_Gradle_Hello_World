74da8dd94a7765e3558bd6afd2066f7a58ecf71f
Ted Lehr <t_l81@txstate.edu>
2019-04-25 08:43:04 -0500

    Updated build.gradle
    Added updated Junit dependencies and explicit test directives build.gradle

--- src/test/java/hello/TestGreeter.java
+++ src/test/java/hello/TestGreeter.java
@@ -8,41 +8,27 @@ import org.junit.jupiter.api.Test;
 
 public class TestGreeter {
 
-
-
    private Greeter g = new Greeter();
 
-	
-
-
-
-
    @Test
-
+   @DisplayName("Test for Empty Name")
    public void testGreeterEmpty() 
 
    {
-
       assertEquals(g.getName(),"");
-
-      assertEquals(g.sayHello(),"Bark!");
-
+      assertEquals(g.sayHello(),"Hello!");
    }
 
-	
+
 
    @Test
-
+   @DisplayName("Test for Name='World'")
    public void testGreeter() 
-
    {
 
       g.setName("World");
-
       assertEquals(g.getName(),"World");
-
       assertEquals(g.sayHello(),"Hello World!");
-
    }
 
 }
