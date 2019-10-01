37f20cc0fc03938abb1ad97a455e064379f545f3
Ted Lehr <t_l81@txstate.edu>
2019-10-01 17:32:02 -0500

    Update TestGreeter.java
    Changed a comment.

--- src/test/java/hello/TestGreeter.java
+++ src/test/java/hello/TestGreeter.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-// Added a comment to kick off a build
+// Change the comment to kick off a build
 
 public class TestGreeter {
 
